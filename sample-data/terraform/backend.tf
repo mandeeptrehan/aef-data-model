e4b6bf2500fd659bb551ea4a4aed3524299144ea
mbawa@google.com <mbawa@google.com>
2025-05-06 16:12:18 +0000

    updates

--- sample-data/terraform/backend.tf
+++ sample-data/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "<TERRAFORM_BUCKET>"
-    prefix = "<TERRAFORM_ENV>"
+    bucket = "aef-pso-chi-training-tfe"
+    prefix = "sample-data/environments/dev"
   }
 }
